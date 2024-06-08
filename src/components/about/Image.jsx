f9e8994576a9183574f4ff7490fe392e9bd22869
alex aku <vixndotcom@gmail.com>
2024-06-08 18:38:44 +0000

    motto-img

--- src/components/about/Image.jsx
+++ src/components/about/Image.jsx
@@ -4,7 +4,7 @@ import { mead } from '../../assets'
 const Image = () => {
   return (
     <section className='mt-5 mb-5'>
-      <div className='w-[50%] h-[50%] mx-auto'>
+      <div className='w-[75%] h-[75%] mx-auto'>
         <img src={mead} alt="" />
       </div>
     </section>
--- src/components/about/Motto.jsx
+++ src/components/about/Motto.jsx
@@ -9,14 +9,14 @@ const Motto = () => {
         <span className='text-[36px] text-black neue-bold '>Titogems Company Limited </span> is a purpose driven company aimed at building mindsets for success and purpose fulfilment.
 
 The company was birthed out of the need to groom young minds and adults alike in discovering who they really are and what makes them uniquely different from others. </p>
-<p className='mt-4'>
+<p className='mt-4 '>
 We are out to ensure that everyone who takes a seat under our auspicious tutelage is fired up and juiced up from within to be the best they can ever be.</p>
 
 
 
 <br />
 
-<h2 className='mt-4 mx-auto text-center neue-bold text-black font-bold'>OUR MOTTO: Success, a must!</h2>
+<h2 className='mt-4 mx-auto text-center neue-bold text-black font-bold'>OUR MOTTO: Success, is a must!</h2>
 
 
         
