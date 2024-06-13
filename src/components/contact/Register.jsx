979a2e7d5d8bd0529ddd3f8c88151ae9cb52a2db
alex aku <vixndotcom@gmail.com>
2024-06-13 20:48:49 +0100

    forms

--- src/components/contact/Register.jsx
+++ src/components/contact/Register.jsx
@@ -34,7 +34,8 @@ const Register = () => {
 
  
     <button onClick={toggleForm} className='neue-ligh font-normal text-[20px] flex'>
-      <button className='mt-4 circle text-white neue-bold mr-4'>01</button> <span className='mt-4 mr-4'>Fill out the form below </span>
+      <button className='mt-4 circle text-white neue-bold mr-4'>01</button>
+       <p className='mt-4 mr-4'>Fill out the form below </p> 
     <img src={showForm ? cancel : open } className='w-[8%] mt-6' />
     </button> 
 
@@ -46,8 +47,8 @@ const Register = () => {
  
    {showForm && (
  
-     <div className={`${styles.flexCenter} justify-content-center mt-10 mb-5 `}>
-       <div className='mx-auto text-center '>
+     <div className={`${styles.flexCenter} justify-content-center mt-10 mb-5  `}>
+       <div className='mx-auto text-center p-4  '>
        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfSHzjLdoicTX1ZOEfGmRkK_zW3s-4-q3cpzGDdg-E9R1egHw/viewform?embedded=true" 
        width="640" height="1280" frameborder="0" marginheight="0" marginwidth="0" className='forms'>Loading Form</iframe> 
 
