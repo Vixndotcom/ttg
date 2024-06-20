0ac767cde1dd58b044b7947cabc81737b3b20b71
alex aku <vixndotcom@gmail.com>
2024-06-20 16:06:56 +0100

    HERO MSG

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
   return (
     <section className=' '> 
     <h1 className='neue-bold text-[36px] text-white text-center justify-content-center mx-auto p-8 track'>
-     <div className=' mt-20 shadow '>DISCOVER YOUR</div>
-     <div className='shadow mt-4'>POTENTIAL, SHAPE</div>
-     <div className='shadow mt-4'>YOUR FUTURE</div>
+     <div className=' mt-20 shadow '>DISCOVER YOUR POTENTIAL,</div>
+     <div className='shadow mt-4'>SHAPE YOUR</div>
+     <div className='shadow mt-4'>FUTURE</div>
     </h1>
     <button className='button text-[20px] text-white text-center mx-auto neue-regular flex flex-row '>
         <Link to ='/register' className='neue-regular text-center flex flex-row  '>Get Started  
